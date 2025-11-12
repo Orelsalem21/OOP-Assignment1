6bb15fc842a92079a665c30128b7f6fed29bb840
Orelsalem21 <Orelsalem21@gmail.com>
2025-11-12 09:06:45 +0200

    1.0

---
+++ src/Point.java
@@ -0,0 +1,20 @@
+public class Point {
+    private double x;
+    private double y;
+
+    // constructor
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+// distance -- return the distance of this point to the other point
+  //  public double distance(Point other) { }
+
+    // equals -- return true is the points are equal, false otherwise
+    //public boolean equals(Point other) { }
+
+    // Return the x and y values of this point
+    //public double getX() { }
+    //public double getY() { }
